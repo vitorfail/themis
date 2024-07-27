ada02986cd997c561c40480126dcf695d9efe649
vitorfail <vitor_andrademanoel@hotmail.com>
2024-07-27 18:42:05 -0300

    tentativa

---
+++ src/app/components/Header/index.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function Headers(){
+
+    return(
+        <header>
+            <span className="icon"></span>
+            <div className="content"></div>
+        </header>
+    )
+}
